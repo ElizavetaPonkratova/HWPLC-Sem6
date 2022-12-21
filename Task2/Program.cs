86d7d4203bfb5ad59bb30a51f7f86fb0d0ec83a3
ElizavetaPonkratova <elizaveta.ski@yandex.ru>
2022-12-21 16:34:30 +0700

    HWSem6-2

---
+++ Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Напишите программу, которая найдет точку пересечения двух прямых, заданных уравнениями:
+y = k1 * x + b1, y = k2 * x +b2.
+Значения k1, b1, k2, b2 задаются пользователем*/
+
+Console.WriteLine("Enter k1: ");
+float k1 = Convert.ToSingle(Console.ReadLine());
+Console.WriteLine("Enter b1: ");
+float b1 = Convert.ToSingle(Console.ReadLine());
+Console.WriteLine("Enter k2: ");
+float k2 = Convert.ToSingle(Console.ReadLine());
+Console.WriteLine("Enter b2: ");
+float b2 = Convert.ToSingle(Console.ReadLine());
+intersectionPoint(k1, b1, k2, b2);
+
+void intersectionPoint(float k1, float b1, float k2, float b2)
+{
+    float x = (b2 - b1) / (k1 - k2);
+    float y = k1 * x + b1;
+    
+    if (k1 == k2)
+    {
+        Console.WriteLine("Straight lines don't intersect");
+    }
+    else
+    {
+        Console.WriteLine($"Intersection at a point X:{x}, Y:{y}");
+    }
+}
