baeaaed4dec09bdea64a893767b2a4ea8ce9008f
ElizavetaPonkratova <elizaveta.ski@yandex.ru>
2022-12-21 16:32:42 +0700

    HWSem6-1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3*/
+
+Console.WriteLine("Введите количество чисел: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] Arr = new int[num];
+int count = 1;
+
+for (int i = 0; i < Arr.Length; i++)
+{
+    Console.Write($"Введите число {count}: ");
+    Arr[i] = Convert.ToInt32(Console.ReadLine());
+    count++;
+}
+
+Console.WriteLine("Числа, которые Вы ввели: ");
+
+for (int i = 0; i < Arr.Length; i++)
+{
+    Console.Write(Arr[i] + " ");
+}
+
+int sum = 0;
+for (int i = 0; i < Arr.Length; i++)
+{
+    if (Arr[i] > 0)
+    {
+        sum++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"Количество значений больше 0 => {sum}");
